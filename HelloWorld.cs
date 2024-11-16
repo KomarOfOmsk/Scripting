7f3413125aed897355b19f58621ae07380d208cb
KomarOfOmsk <makarkuzmin258@gmail.com>
2024-11-16 20:02:38 +0300

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,12 +7,11 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject);
+        print("Hello World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("crickets");
     }
 }
