7764591f4f26b32ee06d500d454b9d59cdcc5f73
KomarOfOmsk <makarkuzmin258@gmail.com>
2024-12-21 22:13:36 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    private int health = 10;
+    public int health = 10;
+    public int maxHealth = 10;
+    public int coins;
 
-    private int coins;
-
+    
     public GameObject fireballPrefab;
     public Transform attackPoint;
+    public AudioSource audioSource;
+    public AudioClip damageSound;
 
-    // Метод изменяющий здоровье игрока
+    // РњРµС‚РѕРґ РёР·РјРµРЅСЏСЋС‰РёР№ Р·РґРѕСЂРѕРІСЊРµ РёРіСЂРѕРєР°
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+        print(health);
+
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+        
     }
 
     public void CollectCoins()
     {
         coins++;
-        print("Собранные монеты: " + coins);
+        print("РЎРѕР±СЂР°РЅРЅС‹Рµ РјРѕРЅРµС‚С‹: " + coins);
     }
     void Update()
     {
